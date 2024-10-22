c5e6fe9ce3d652d35b3b6ec866fdb1950679a6b1
KentoShiba <kento.shiba@student.manchester.ac.uk>
2024-10-22 10:23:24 +0100

    analyse_sipm works now

---
+++ My code/configuration.py
@@ -0,0 +1,7 @@
+#change these folders depending on where you want to save data and plots
+DATA_FOLDER = r"C:\Users\kento\Documents\University of Manchester\University Work\Fourth Year\MPhys\MPhys---VUV-Photosensor-Testing-for-Neutrino-and-Dark-Matter-Detectors\My code\data\\"
+PLOTS_FOLDER = r"C:\Users\kento\Documents\University of Manchester\University Work\Fourth Year\MPhys\MPhys---VUV-Photosensor-Testing-for-Neutrino-and-Dark-Matter-Detectors\My code\plots\\"
+RESULTS_FOLDER = r"C:\Users\kento\Documents\University of Manchester\University Work\Fourth Year\MPhys\MPhys---VUV-Photosensor-Testing-for-Neutrino-and-Dark-Matter-Detectors\My code\results\\"
+
+LOC_DATA_SIPM = r"C:\Users\kento\Documents\University of Manchester\University Work\Fourth Year\MPhys\MPhys---VUV-Photosensor-Testing-for-Neutrino-and-Dark-Matter-Detectors\My code\Data_SiPM\\"
+LOC_DATA_PMT = r"C:\Users\kento\Documents\University of Manchester\University Work\Fourth Year\MPhys\MPhys---VUV-Photosensor-Testing-for-Neutrino-and-Dark-Matter-Detectors\My code\Data_PMT\\"
